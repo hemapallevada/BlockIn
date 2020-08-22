aac9d2bf1325da0d5945e49691e4cf9139884a75
hemapallevada <57134197+hemapallevada@users.noreply.github.com>
2020-08-22 18:08:20 +0530

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.6;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+contract MyToken is  ERC20 {
+    address public minter;
+    constructor()ERC20("Mytoken","$") public{
+       _mint(msg.sender,21000000);
+       minter=msg.sender;
+     
+    }
+    
+     
+}
