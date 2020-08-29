741a45de21c3ef17892b0976d474ba57ae084e51
hemapallevada <57134197+hemapallevada@users.noreply.github.com>
2020-08-29 10:27:08 +0530

    Update MyToken.sol
    Added buyTokens

--- MyToken.sol
+++ MyToken.sol
@@ -7,6 +7,16 @@ contract MyToken is  ERC20 {
        minter=msg.sender;
      
     }
+    function buyTokens(address payable minter_add,uint tokens_to_purchase) payable public{
+       uint amount_required=tokens_to_purchase*2;
+        require(msg.value>=amount_required,"Please send enough money to buy tokens");
+        uint spend=amount_required;
+       
+        minter_add.transfer(spend);
+         
+        transferFrom(minter,msg.sender,tokens_to_purchase);
+
+    }
   function getMinter() public view returns(address){
   return minter;
   }
